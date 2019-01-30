7854424f6d9bf029cfe9cd49da6bb803223f0c9e
Romulo Sousa <romulosousa27@hotmail.com>
2019-01-29 22:13:36 -0300

    Products API

---
+++ app/Models/Product.php
@@ -0,0 +1,11 @@
+@case('view')
+@include($view)
+@break
+
+
+$inputs [] = [
+            'name' => 'groups',
+            'label' => 'Condições',
+            'type' => 'view',
+            'view' => 'brand.motorcycle.variations.financing.field-groups'
+        ];
